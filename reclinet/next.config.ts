4a4d61151362d5f5210c50fbda240b5b6da116ed
ishikabhoyar <ishikabhoyar2005@gmail.com>
2025-08-24 01:02:56 +0530

    fix: remove ESLint and TypeScript configuration from next.config.ts and update build script

--- reclinet/next.config.ts
+++ reclinet/next.config.ts
@@ -10,12 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true, // 🚀 disables ESLint in builds
-  },
-  typescript: {
-    ignoreBuildErrors: true, // 🚀 disables TS checks in builds
-  },
 };
 
 export default nextConfig;
